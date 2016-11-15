a428593ae318ca10b5f94ece23325c1e70f21058
Brian Francisco <ibrianfrancisco@Gretchens-MacBook-Pro.local>
2016-11-14 20:13:09 -0800

    addde if statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,6 +3,12 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardOne) {
-	window.alert("Wrong");
-} 
+if (cardTwo === cardFour) {
+	window.alert("Sorry, try again.");
+} else if (cardOne === cardTwo) {
+	window.alert("You found a match!");
+} else if (cardThree === cardFour) {
+	window.alert("You found a match!");
+} else {
+	window.alert("pick something");
+}
