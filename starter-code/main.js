c139c4881fc0a2b5b550211cf3df78cb976a3cec
Brian <ibrianfrancisco@gmail.com>
2016-11-24 14:48:30 -0800

    Added isTwoCards and isMatch functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,64 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
 
-if (cardTwo === cardFour) {
-	window.alert("Sorry, try again.");
-} else if (cardOne === cardTwo) {
-	window.alert("You found a match!");
-} else if (cardThree === cardFour) {
-	window.alert("You found a match!");
-} else {
-	window.alert("pick something");
-}
+var cardsInPlay = [];
+
+var gameBoard = document.getElementById('game-board');
+
+
+
+var createBoard = function() {
+	// this creates the loop to make the 4 div elements
+	for (var i = 0; i < cards.length; i++) {
+		// this creates the code for the div elements
+		var cardElement = document.createElement('div');
+		// this gives the just created div elements the
+		// class name of card
+		cardElement.className = 'card';
+		// this attaches the four div elements to the
+		// parent element of the id game-board
+		gameBoard.appendChild(cardElement);
+ 		// this will set the card's 'data-card' to be the element of the array
+ 		// data- attributes are meant to store data about an element that is not
+ 		// tied to a style.
+ 		// i.e. "king"
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards)
+	}
+};
+
+
+var isTwoCards = function() {
+  	// add card to array of cardsInPlay
+  	// 'this' hasn't been covered in this prework, but
+  	// for now, just know it gives you access to the card the user clicked on
+  	cardsInPlay.push(this.getAttribute('data-card'));
+
+  	// if you have two cards in play check for a match
+  	if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+	}
+};
+
+
+
+var isMatch = function(cardChosen) {
+	if (cardChosen[0] === cardChosen[1]) {
+	window.alert("You found a match!")
+ 	} else if (cardChosen[0] !== cardChosen[1]) {
+ 	window.alert("Sorry, try again.")
+ 	}
+ };
+
+
+createBoard ();
+
+
+
+
+
+
